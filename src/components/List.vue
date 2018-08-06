052d5fe884365b57485d8f5473b7a22a0aece5b9
ellen <ellen@brandma.co>
2018-08-06 16:47:00 +0800

    first commit

---
+++ src/components/List.vue
@@ -0,0 +1,19 @@
+<template>
+<div> {{msg}} </div>
+</template>
+
+<script>
+export default {
+  name: 'List',
+  data () {
+    return {
+      msg: 'list content'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
